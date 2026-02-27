6aff467f7b6cd38cffad1ed90f80fdc56fd6b8d1
Nagaraju Ekkirala <99072240+nagarajuekkirala@users.noreply.github.com>
2026-02-27 15:52:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,49 +1,42 @@
-from langchain_openai import ChatOpenAI
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import ChatOpenAI,OpenAIEmbeddings
+from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 import streamlit as st
-import os
+
 from dotenv import load_dotenv
+import os
 load_dotenv()
-key = os.getenv('OPENAI_API_KEY')
 
-question = st.text_area("Enter the Question: ")
+key = os.getenv('OPEN_API_KEY')
 
-model = ChatOpenAI(api_key=key,
-                   model = 'gpt-4.1-mini',
-                   temperature=0.5)
+loader = PyPDFDirectoryLoader(r'D:\ML_DSNR')
+doc = loader.load()
 
-prompt = ChatPromptTemplate(("human", """You are given a user question. Your tasks:
 
-1. Classify the question into exactly one of the following categories:
-   - Python
-   - SQL
-   - Power BI
-   - Machine Learning
+splitter = RecursiveCharacterTextSplitter(chunk_size = 1000,chunk_overlap = 200)
+documents = splitter.split_documents(doc)
 
-2. Extract the most important keywords from the question.
-   - Keywords should be relevant technical terms, functions, concepts, or operations.
-   - Provide 3–4 keywords depending on question length.
+embedding = OpenAIEmbeddings(api_key=key)
+vector_store = Chroma(embedding_function=embedding)
+vector_store.add_documents(doc)
+retriever = vector_store.as_retriever()
 
-3. If unclear, choose the closest matching category based on context.
+prompt = ChatPromptTemplate.from_template("""Based on user question give the correct answer based only on context
+                                 question:{question}
+                                 context:{context}""")
 
-Return the output in the following JSON format:
+llm = ChatOpenAI(api_key=key,model='gpt-4o-mini',temperature=0)
 
-{{
-  "category": "<Python | SQL | Power BI | Machine Learning |Large Language Model |Agents>",
-  "keywords": ["keyword1", "keyword2", ...],
-   'Answer': 'Explain about question in 100-200 tokens  ',
-    'Sample Code': 'Explain simple python code snippet',
-   'Important Links': 'Provide research papers/youtube links'                                   
-                                           
-}}
+rag_chain = ({'context':retriever,"question":RunnablePassthrough()} | prompt | llm | StrOutputParser() )
 
-Here is the question:
-{question}
-""")
-    )
+st.image(r'https://innomatics.in/wp-content/uploads/2023/01/innomatics-footer-logo.png')
 
-chain = prompt | model
+query = st.text_input("Enter the Query")
 
-if st.button("Submit"):
-    response = chain.invoke({'question':question})
-    st.json(response.content)
+if st.button("Predict"):
+    resposne = rag_chain.invoke(query)
+    st.write(resposne)
